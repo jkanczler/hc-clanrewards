b3757e37a3c4ebd1c26cedbe723ba82da2c2fcff
jkanczler <jkanczle@itron.com>
2022-07-04 02:26:24 +0200

    v0.1

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,72 @@
 import sys
+import json
+from data.clanmates import clan_mates
+from data.items import items
 
+def sort_clan_mates():
+    clan_mates.sort(key=lambda cm: cm['glory'], reverse=True)
+
+def get_next_buyer():
+    sort_clan_mates()
+
+    for clan_mate in clan_mates:
+        if len(clan_mate['purchased']) < 5:
+            return clan_mate
+
+    return None
+
+def get_item_to_buy(demands):
+    for demand in demands:
+        if demand['quantity'] > 0:
+            for item in items:
+                if demand['name'] == item:
+                    if items[demand['name']] > 0:
+                        return item
+
+    return None
+
+def buy_item(buyer, item_to_buy):
+    print(f"{buyer['name']} is buying '{item_to_buy}'.")
+    print(f"{buyer['glory']} - 50000 = {buyer['glory'] - 50000}")
+
+    if item_to_buy is not None:
+        items[item_to_buy] -= 1
+
+    buyer['purchased'].append(item_to_buy)
+    buyer['glory'] -= 50000
+
+    for demand in buyer['demands']:
+        if demand['name'] == item_to_buy:
+            demand['quantity'] -= 1
+
+def validate_items():
+    for clan_mate in clan_mates:
+        for demand in clan_mate['demands']:
+            if not items.get(demand['name']):
+                print(f"The demand '{demand}' is not in the item list.")
 
 def main() -> int:
-    print('main')
+    validate_items()
 
+    orders = []
+
+    next_buyer = get_next_buyer()
+    while next_buyer is not None:
+        item_to_buy = get_item_to_buy(next_buyer['demands'])
+        buy_item(next_buyer, item_to_buy)
+
+        orders.append(
+            {
+                'name': next_buyer['name'],
+                'purchased': item_to_buy
+            })
+
+        print(f"remaining gloriy is {next_buyer['glory']}")
+
+        next_buyer = get_next_buyer()
+
+    print(json.dumps(clan_mates, indent=2))
+    # print(json.dumps(orders, indent=2))
 
 if __name__ == '__main__':
     sys.exit(main())
---
+++ src/data/clanmates.py
@@ -0,0 +1,180 @@
+clan_mates = [
+    {
+        'name': 'amare',
+        'glory': 446636,
+        'demands': [
+            { 'name': 'icy_potion', 'quantity': 1 },
+            { 'name': 'crystallite', 'quantity': 1 },
+            { 'name': 'damage_elixir', 'quantity': 1 },
+            { 'name': 'pierce_elixir', 'quantity': 1 },
+            { 'name': 'dia', 'quantity': 1 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'Goku',
+        'glory': 259748,
+        'demands': [
+            { 'name': 'damage_elixir', 'quantity': 10 },
+            { 'name': 'lego_book', 'quantity': 10 },
+            { 'name': 'dia', 'quantity': 10 },
+            { 'name': 'ticket', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10}
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'Juckos',
+        'glory': 215962,
+        'demands': [
+            { 'name': 'predators_eye', 'quantity': 10 },
+            { 'name': 'clan_coin', 'quantity': 10 },
+            { 'name': 'lego_book', 'quantity': 10 },
+            { 'name': 'ticket', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'Nasde',
+        'glory': 213469,
+        'demands': [
+            { 'name': 'lego_book', 'quantity': 1 },
+            { 'name': 'predators_eye', 'quantity': 1 },
+            { 'name': 'lego_book', 'quantity': 1 },
+            { 'name': 'clan_coin', 'quantity': 10 },
+            { 'name': 'damage_elixir', 'quantity': 1 },
+            { 'name': 'premium', 'quantity': 1 },
+            { 'name': 'ticket', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10 },
+            { 'name': 'ether', 'quantity': 10}
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'ooRICSKOoo',
+        'glory': 156899,
+        'demands': [
+            { 'name': 'lego_book', 'quantity': 10 },
+            { 'name': 'damage_elixir', 'quantity': 10 },
+            { 'name': 'clan_coin', 'quantity': 10 },
+            { 'name': 'ticket', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10 },
+            { 'name': 'torch', 'quantity': 10 },
+            { 'name': 'ether', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'Ayrisz',
+        'glory': 192503,
+        'demands': [
+            { 'name': 'clan_coin', 'quantity': 10 },
+            { 'name': 'lego_book', 'quantity': 10 },
+            { 'name': 'torch', 'quantity': 10 },
+            { 'name': 'dia', 'quantity': 10 },
+            { 'name': 'ticket', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'KisCsako88',
+        'glory': 139290,
+        'demands': [
+            { 'name': 'dragon_scale', 'quantity': 10 },
+            { 'name': 'clan_coin', 'quantity': 10 },
+            { 'name': 'dia', 'quantity': 10 },
+            { 'name': 'kerchief_of_swiftness', 'quantity': 10 },
+            { 'name': 'scroll_of_illusions', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10 },
+            { 'name': 'ticket', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'Suzie',
+        'glory': 78926,
+        'demands': [
+            { 'name': 'premium', 'quantity': 10 },
+            { 'name': 'dia', 'quantity': 10 },
+            { 'name': 'torch', 'quantity': 10 },
+            { 'name': 'ether', 'quantity': 10 },
+            { 'name': 'purple_shard', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'api',
+        'glory': 67884,
+        'demands': [
+            { 'name': 'clan_coin', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10 },
+            { 'name': 'dia', 'quantity': 10 },
+            { 'name': 'torch', 'quantity': 10 },
+            { 'name': 'ether', 'quantity': 10 },
+            { 'name': 'purple_shard', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'Chee',
+        'glory': 177389,
+        'demands': [
+            { 'name': 'lego_book', 'quantity': 10 },
+            { 'name': 'dia', 'quantity': 10 },
+            { 'name': 'icy_potion', 'quantity': 10 },
+            { 'name': 'damage_elixir', 'quantity': 10 },
+            { 'name': 'pierce_elixir', 'quantity': 10 },
+            { 'name': 'armor_elixir', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10 },
+            { 'name': 'ether_catcher', 'quantity': 10 },
+            { 'name': 'purple_shard', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'Chee the 3rd',
+        'glory': 92786,
+        'demands': [
+            { 'name': 'premium', 'quantity': 10 },
+            { 'name': 'kerchief_of_swiftness', 'quantity': 10 },
+            { 'name': 'damage_elixir', 'quantity': 10 },
+            { 'name': 'pierce_elixir', 'quantity': 10 },
+            { 'name': 'armor_elixir', 'quantity': 10 },
+            { 'name': 'fortitude_elixir', 'quantity': 10 },
+            { 'name': 'clan_coin', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10 },
+            { 'name': 'goblin', 'quantity': 10 },
+            { 'name': 'purple_shard', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'robert',
+        'glory': 106614,
+        'demands': [
+            { 'name': 'dia', 'quantity': 1 },
+            { 'name': 'lego_book', 'quantity': 1 },
+            { 'name': 'badge', 'quantity': 1 },
+            { 'name': 'icy_potion', 'quantity': 1 },
+            { 'name': 'fireball', 'quantity': 1 },
+            { 'name': 'badge', 'quantity': 1 },
+            { 'name': 'clan_coin', 'quantity': 1 },
+            { 'name': 'ether', 'quantity': 10 },
+            { 'name': 'purple_shard', 'quantity': 10 }
+        ],
+        'purchased': []
+    },
+    {
+        'name': 'Josh',
+        'glory': 121487,
+        'demands': [
+            { 'name': 'clan_coin', 'quantity': 10 },
+            { 'name': 'dia', 'quantity': 10 },
+            { 'name': 'ticket', 'quantity': 10 },
+            { 'name': 'badge', 'quantity': 10 },
+            { 'name': 'ether_catcher', 'quantity': 10 },
+        ],
+        'purchased': []
+    }
+]
