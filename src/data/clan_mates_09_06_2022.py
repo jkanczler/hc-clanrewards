e60c978d8cc13f2858ebfca32ce942ebe4758c41
jkanczler <jkanczle@itron.com>
2022-09-06 11:23:54 +0200

    new data files

---
+++ src/data/clan_mates_09_06_2022.py
@@ -0,0 +1,152 @@
+clan_mates = [
+    {
+        'name': 'Ayrisz',
+        'glory': 235398,
+        'demands': [
+            { 'items': ['predators_eye'], 'quantity': 1 },
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['clan_coin'], 'quantity': 2 },
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 5 },
+            { 'items': ['ticket'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'Amare',
+        'glory': 222855,
+        'demands': [
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 1 },
+            { 'items': ['ticket'], 'quantity': 1 },
+            { 'items': ['elixir_health'], 'quantity': 1 },
+            { 'items': ['ether', 'torch'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'Juckos',
+        'glory': 195320,
+        'demands': [
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 2 },
+            { 'items': ['clan_coin'], 'quantity': 2 },
+        ],
+    },
+    {
+        'name': 'KisCsako88',
+        'glory': 172484,
+        'demands': [
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 4 },
+            { 'items': ['gem_health'], 'quantity': 1 },
+            { 'items': ['premium'], 'quantity': 2 },
+            { 'items': ['ticket'], 'quantity': 2 },
+            { 'items': ['badge'], 'quantity': 1 },
+        ],
+    },
+    {
+        'name': 'Nasde',
+        'glory': 129665,
+        'demands': [
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['goblin'], 'quantity': 2 },
+            { 'items': ['clan_coin'], 'quantity': 2 },
+            { 'items': ['premium'], 'quantity': 5 },
+            { 'items': ['ether', 'torch'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'Chee',
+        'glory': 126341,
+        'demands': [
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['elixir_health', 'elixir_pierce', 'elixir_accu', 'elixir_magic_pierce', 'elixir_sp', 'elixir_magic_armor'], 'quantity': 2 },
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 1 },
+            { 'items': ['badge'], 'quantity': 2 },
+            { 'items': ['ticket'], 'quantity': 2 },
+            { 'items': ['shard_3_epic'], 'quantity': 1 },
+        ],
+    },
+    {
+        'name': 'emmi90',
+        'glory': 119226,
+        'demands': [
+            { 'items': ['predators_eye'], 'quantity': 1 },
+            { 'items': ['leather_glove'], 'quantity': 1 },
+            { 'items': ['premium'], 'quantity': 5 },
+            { 'items': ['clan_coin'], 'quantity': 1 },
+            { 'items': ['spirit_lego'], 'quantity': 1 },
+            { 'items': ['ether', 'torch'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'robert',
+        'glory': 109810,
+        'demands': [
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 1 },
+            { 'items': ['badge'], 'quantity': 3 },
+            { 'items': ['spirit_epic'], 'quantity': 1 },
+            { 'items': ['ether', 'torch'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'Josh',
+        'glory': 102589,
+        'demands': [
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 1 },
+            { 'items': ['elixir_health', 'elixir_pierce', 'elixir_accu', 'elixir_magic_pierce', 'elixir_sp', 'elixir_magic_armor'], 'quantity': 2 },
+            { 'items': ['totem_lego', 'totem_epic'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'Chee the 3rd',
+        'glory': 62196,
+        'demands': [
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['elixir_health', 'elixir_pierce', 'elixir_accu', 'elixir_magic_pierce', 'elixir_sp', 'elixir_magic_armor'], 'quantity': 2 },
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 1 },
+            { 'items': ['badge'], 'quantity': 2 },
+            { 'items': ['ticket'], 'quantity': 2 },
+            { 'items': ['shard_3_epic'], 'quantity': 1 },
+        ],
+    },
+    {
+        'name': 'Goku',
+        'glory': 55742,
+        'demands': [
+            { 'items': ['clan_coin'], 'quantity': 5 },
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 5 },
+            { 'items': ['ticket'], 'quantity': 5 },
+            { 'items': ['ether', 'torch'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'Gizi',
+        'glory': 55408,
+        'demands': [
+            { 'items': ['premium'], 'quantity': 1 },
+            { 'items': ['goblin'], 'quantity': 1 },
+            { 'items': ['totem_lego', 'totem_epic'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'Api',
+        'glory': 40726,
+        'demands': [
+            { 'items': ['dia_100', 'dia_50'], 'quantity': 1 },
+            { 'items': ['badge'], 'quantity': 1 },
+            { 'items': ['gem_dodge'], 'quantity': 1 },
+            { 'items': ['goblin'], 'quantity': 1 },
+            { 'items': ['spirit_lego', 'spirit_epic'], 'quantity': 2 },
+            { 'items': ['totem_lego', 'totem_epic'], 'quantity': 5 },
+        ],
+    },
+    {
+        'name': 'Libra',
+        'glory': 38264,
+        'demands': [
+            { 'items': ['premium'], 'quantity': 1 },
+            { 'items': ['goblin'], 'quantity': 1 },
+            { 'items': ['totem_lego', 'totem_epic'], 'quantity': 5 },
+        ],
+    },
+]
